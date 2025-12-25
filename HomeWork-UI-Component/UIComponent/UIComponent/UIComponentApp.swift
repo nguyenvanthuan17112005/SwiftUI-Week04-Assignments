07773232168acb66994b2b912ced5615ad3ffcab
nguyenvanthuan17112005 <thuannv2563@ut.edu.vn>
2025-12-25 22:43:13 +0700

    all commit

---
+++ HomeWork-UI-Component/UIComponent/UIComponent/UIComponentApp.swift
@@ -0,0 +1,17 @@
+//
+//  UIComponentApp.swift
+//  UIComponent
+//
+//  Created by Nguyễn Văn Thuận on 24/12/25.
+//
+
+import SwiftUI
+
+@main
+struct UIComponentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
